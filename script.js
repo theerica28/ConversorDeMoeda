e8c5e8a769e43d252665d55a7107ab1c6f3f0ba9
Érica Santos <104101478+theerica28@users.noreply.github.com>
2023-11-13 10:24:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Estamos começando a imersão');
+
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.32;
+
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+alert('R$ '+valorEmReal);
